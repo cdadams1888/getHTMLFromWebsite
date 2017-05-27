35c317763ee03b464ac4cca09749818f68b2e47a
Christopher Adams <cdadams1888@yahoo.com>
2017-05-27 19:44:17 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import https from 'https';
 
 https.get('https://www.lynda.com', res => {
-   console.log('Resppnse status code: ', res.statusCode);
+   console.log('Response status code: ', res.statusCode);
 
    res.on('data', chunk => {
       console.log(chunk.toString());
